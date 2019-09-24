fa848f8f00e7afe5c1db8a5ed9d18103db3953d7
Brandon <brandonlang1997@gmail.com>
2019-09-24 17:03:47 -0600

    updating

--- sever.js
+++ sever.js
@@ -28,10 +28,11 @@ db.once("open", function() {
 });
 
 var app = express();
+app.use(express.static("public"));
 var port = process.env.PORT || 3000;
 
 app.use(logger("dev"));
-app.use(express.static("/public"));
+// app.use(express.static("/public"));
 app.use(body.urlencoded({ extended: false }));
 app.use(method("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -56,43 +57,48 @@ app.get("/", function(req, res) {
 });
 
 app.get("/scrape", function(req, res) {
-  request("https://www.nytimes.com/section/world", function(
+  request("https://www.npr.org/sections/news/", function(
     error,
     response,
     html
   ) {
     var $ = cheerio.load(html);
     var result = {};
-    $("div.story-body").each(function(i, element) {
+    $(".item").each(function(i, element) {
+      // console.log("testing");
       var link = $(element)
-        .find("a")
+        .find(".title")
+        .children("a")
         .attr("href");
+        console.log(link)
       var title = $(element)
-        .find("h2.headline")
-        .text()
-        .trim();
+      .find(".title")        
+      .text()
+      .trim();
+      console.log(title);
       var summary = $(element)
-        .find("p.summary")
+        .find(".teaser")
         .text()
         .trim();
-      var img = $(element)
-        .parent()
-        .find("figure.media")
-        .find("img")
-        .attr("src");
+        console.log(summary)
+      // var img = $(element)
+      //   .parent()
+      //   .find("")
+      //   .find("")
+      //   .attr("");
       result.link = link;
       result.title = title;
       if (summary) {
         result.summary = summary;
       }
-      if (img) {
-        result.img = img;
-      } else {
-        result.img = $(element)
-          .find(".wide-thumb")
-          .find("img")
-          .attr("src");
-      }
+      // if (img) {
+      //   result.img = img;
+      // } else {
+      //   result.img = $(element)
+      //     .find(".wide-thumb")
+      //     .find("img")
+      //     .attr("src");
+      // }
       var entry = new Article(result);
       Article.find({ title: result.title }, function(err, data) {
         if (data.length === 0) {
